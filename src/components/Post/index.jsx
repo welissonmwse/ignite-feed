f972b3a594d572b7403a52d19615daa5a8cdc8d4
Welisson Silveira <welissonmwse@gmail.com>
2022-06-03 09:50:14 -0300

    Adiciona seção de adicionar comentário ao  componente Post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -14,11 +14,20 @@ export function Post(){
         <time title="03 de Junho as 08:41h" dateTime="2022-06-03 08:41:00">Públicado há 1h</time>
       </header>
       <div className={styles.content}>
-      <p>Fala galeraa 👋</p>
-      <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-      <p>👉 <a href=''>jane.design/doctorcare</a></p>
-      <p><a href=''>#novoprojeto</a><a href=''> #nlw </a> <a href=''> #rocketseat</a></p>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 <a href=''>jane.design/doctorcare</a></p>
+        <p><a href=''>#novoprojeto</a><a href=''> #nlw </a> <a href=''> #rocketseat</a></p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comnetário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
