1831ed044ec0bb86b6538d5ff69673bd1c064536
Welisson Silveira <welissonmwse@gmail.com>
2022-06-14 18:51:34 -0300

    adicionando typescript ao component Post

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,12 +1,29 @@
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
-import { useState } from 'react'
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
 import { Avatar } from '../Avatar'
 import { Comment } from '../Comment'
 import styles from './styles.module.css'
 
-export function Post({author, content, publishedAt}){
-  const [comments, setComments] = useState([])
+interface Author{
+  name: string;
+  role: string;
+  avatarUrl: string;
+}
+
+interface Content{
+  type: 'paragraph' | 'link';
+  content: string;
+}
+
+interface PostProps{
+  author: Author;
+  publishedAt: Date;
+  content: Content[];
+}
+
+export function Post({author, content, publishedAt}: PostProps){
+  const [comments, setComments] = useState([''])
   const [newCommentText, setNewCommentText] = useState('')
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
@@ -18,22 +35,22 @@ export function Post({author, content, publishedAt}){
     addSuffix: true,
   })
 
-  function handleCreateNewComment(event){
+  function handleCreateNewComment(event: FormEvent){
     event.preventDefault()
     setComments([...comments, newCommentText])
     setNewCommentText('')
   }
 
-  function handleNewCommentChange(event) {
+  function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
     event.target.setCustomValidity('')
     setNewCommentText(event.target.value)
   }
 
-  function handleNewCommentInvalid(event) {
+  function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
     event.target.setCustomValidity('Esse campo é obrigatório!')
   }
 
-  function deleteComment(commentToDelete) {
+  function deleteComment(commentToDelete: string) {
     const commentWithoutDeletedOne = comments.filter(comment => comment !== commentToDelete)
     setComments(commentWithoutDeletedOne)
   }
