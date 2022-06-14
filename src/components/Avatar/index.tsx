cc6456d61f480d47ec41e571fd946ec565ded0ba
Welisson Silveira <welissonmwse@gmail.com>
2022-06-14 18:59:53 -0300

    adicionando typescript ao component Avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,10 +1,17 @@
 import styles from './styles.module.css'
 
-export function Avatar({hasBorder = true, src}){
+interface AvatarProps{
+  hasBorder?: boolean;
+  src: string;
+  alt?: string;
+}
+
+export function Avatar({hasBorder = true, src, alt}: AvatarProps){
   return(
     <img 
       className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
       src={src} 
+      alt={alt}
     />
   )
 }
